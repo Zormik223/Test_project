c58a70fd90341c9e94a67a5357b54f81f60ec8ef
Zormik223 <zormik@list.ru>
2022-12-26 13:59:00 +0300

    commit

--- conftest.py
+++ conftest.py
@@ -1,12 +1,24 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-@pytest.mark.parametrize('language', ["en"])
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose browser: en or ru")
+
 @pytest.fixture(scope="function")
-def browser(language):
-    print("\nstart browser for test..")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
     browser = webdriver.Chrome()
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
